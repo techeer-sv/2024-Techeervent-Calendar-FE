b1db366833f0f78daaaa7bd983ad30c81248e1a4
seunghwan <sun1238799@naver.com>
2024-12-04 23:22:53 +0900

    chore: 폰트 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -5,6 +5,12 @@ const Home = () => {
     <div>
       <p className="text-blue-500">홈</p>
       <div class="bg-mainBackground text-mainText">메인 페이지</div>
+      <div className="font-pretendard text-lg text-gray-800">
+        Hello, Pretendard font in Tailwind CSS!
+      </div>
+      <div className="font-jua text-xl text-gray-600">
+        Hello, Jua font in Tailwind CSS!
+      </div>
     </div>
   );
 };
