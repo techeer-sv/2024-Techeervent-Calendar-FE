441eb67270dac6aa65d1d92ed30b548fa95fd143
jinoo <jinoo0306@naver.com>
2024-12-05 22:36:23 +0900

    feat : layout.tsx 경로 수정

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Layout from './layout.tsx';
+import Layout from './Layout.tsx';
 
 const queryClient = new QueryClient();
 
