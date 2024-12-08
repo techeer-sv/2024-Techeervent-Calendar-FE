164ae425b8f9c3fb6ec6aa82dd98a1d15e79a703
seunghwan <sun1238799@naver.com>
2024-12-08 19:43:11 +0900

    refactor: 버튼 cursor-pointer 추가

--- src/pages/Home/components/QuestionCheckModal.tsx
+++ src/pages/Home/components/QuestionCheckModal.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import QandA from '../../../assets/images/QandA.png';
 import React from 'react';
 
 interface QuestionModalProps {
@@ -9,7 +10,7 @@ const QuestionCheckModal = ({ onClose }: QuestionModalProps) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="w-modal relative min-h-modal p-4  rounded-[10px] bg-modalWhite flex flex-col items-center justify-center">
-        <div className="w-[118px] h-[116px] bg-redText">눈 내리니는 사진</div>
+        <img className="w-[80px] h-[70px]" src={QandA} />
         <h1 className="font-semibold text-[14px] mt-[32px] mb-[10px]">
           Q) 2024년, 나를 가장 많이 웃게 한 일은?
         </h1>
@@ -18,7 +19,7 @@ const QuestionCheckModal = ({ onClose }: QuestionModalProps) => {
         </div>
         <Button
           onClick={onClose}
-          className="w-full text-[16px] rounded-[999px] bg-modalButtonBackground my-[5px] text-whiteDefault box-border hover:opacity-70"
+          className="w-full text-[16px] rounded-[999px] cursor-pointer bg-modalButtonBackground my-[5px] text-whiteDefault box-border hover:opacity-70"
         >
           닫기
         </Button>
--- src/pages/Home/components/UnableConfirmModal.tsx
+++ src/pages/Home/components/UnableConfirmModal.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/button';
+import Warnning from '../../../assets/images/warning.png';
 import React from 'react';
 
 interface UnableConfirmModalProps {
@@ -9,7 +10,7 @@ const UnableConfirmModal = ({ onSubmit }: UnableConfirmModalProps) => {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
       <div className="w-modal min-h-modal p-4  rounded-[10px] bg-modalWhite flex flex-col items-center justify-center">
-        <div className="w-[90px] h-[90px] bg-redText">눈 내리니는 사진</div>
+        <img className="w-[90px] h-[90px]" src={Warnning} />
 
         <h1 className="mt-[50px] text-[18px] text-center font-medium">
           아직은 내용을 확인할 수 없습니다 !
@@ -19,7 +20,7 @@ const UnableConfirmModal = ({ onSubmit }: UnableConfirmModalProps) => {
         </span>
         <Button
           onClick={onSubmit}
-          className="w-full text-[16px] rounded-[999px] bg-modalButtonBackground text-whiteDefault box-border hover:opacity-70"
+          className="w-full text-[16px] rounded-[999px] cursor-pointer bg-modalButtonBackground text-whiteDefault box-border hover:opacity-70"
         >
           닫기
         </Button>
