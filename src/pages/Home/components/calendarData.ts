bd787be6bebdb2d52d37cd78a0efd1c630aa2b99
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-09 15:52:48 +0900

    fix: 창문 위치 재정렬

--- src/pages/Home/components/calendarData.ts
+++ src/pages/Home/components/calendarData.ts
@@ -8,8 +8,8 @@ import GiftBox7 from '../../../../public/assets/Box7.svg';
 
 const calendarData = [
   { date: 25, x: '42%', y: '7.5%', isOpen: false, giftImage: GiftBox1 },
-  { date: 26, x: '73.5%', y: '7%', isOpen: false, giftImage: GiftBox2 },
-  { date: 27, x: '9%', y: '31%', isOpen: false, giftImage: GiftBox3 },
+  { date: 26, x: '73.5%', y: '6.3%', isOpen: false, giftImage: GiftBox2 },
+  { date: 27, x: '9%', y: '30.5%', isOpen: false, giftImage: GiftBox3 },
   { date: 28, x: '42%', y: '31%', isOpen: false, giftImage: GiftBox4 },
   { date: 29, x: '9%', y: '53%', isOpen: false, giftImage: GiftBox5 },
   { date: 30, x: '42%', y: '50%', isOpen: false, giftImage: GiftBox6 },
