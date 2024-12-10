d7b54b3b17e61f3d460c72e4d12aadd1f1d0f53a
jinoo <jinoo0306@naver.com>
2024-12-11 03:53:51 +0900

    feat: 질문 목록 조회 API 호출 함수 추가

---
+++ src/services/api/question.ts
@@ -0,0 +1,11 @@
+import axiosClient from '@/services/axios.config';
+import { GetRandomQuestionResponse } from '@/types/api/question';
+
+export const fetchRandomQuestion = async (
+  userId: number
+): Promise<GetRandomQuestionResponse> => {
+  const response = await axiosClient.get('/question', {
+    params: { userId },
+  });
+  return response.data;
+};
