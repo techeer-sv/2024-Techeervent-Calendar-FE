6ff3045826b8aba53cd2ffae5fea83686df83dee
jinoo <jinoo0306@naver.com>
2024-12-11 03:52:24 +0900

    feat: 캘린더 관련 API 호출 함수 추가

---
+++ src/services/api/calendar.ts
@@ -0,0 +1,30 @@
+import axiosClient from '@/services/axios.config';
+import {
+  GetUserQAResponse,
+  GetAnswerCountResponse,
+} from '@/types/api/calendar';
+
+export const fetchUserQA = async (
+  userId: number
+): Promise<GetUserQAResponse> => {
+  const response = await axiosClient.get('/calendar', {
+    params: { userId },
+  });
+  return response.data;
+};
+
+export const searchUserQA = async (
+  offset: number,
+  limit: number,
+  author: string
+): Promise<GetUserQAResponse> => {
+  const response = await axiosClient.get('/calender/answer', {
+    params: { offset, limit, author },
+  });
+  return response.data;
+};
+
+export const fetchAnswerCount = async (): Promise<GetAnswerCountResponse> => {
+  const response = await axiosClient.get('/calender/answer/count');
+  return response.data;
+};
