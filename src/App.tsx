8c061e7e83d64f1210c488aef4556015654dc790
jinoo <jinoo0306@naver.com>
2024-12-11 18:14:07 +0900

    feat : NotFound 페이지 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
