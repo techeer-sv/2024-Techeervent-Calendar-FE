444ef5282a88f416d51acd794b858a98fcd530b7
seunghwan <sun1238799@naver.com>
2024-12-12 01:47:20 +0900

    chore: DynamicIcon 파일 주석 제거

--- src/components/icon/DynamicIcon.tsx
+++ src/components/icon/DynamicIcon.tsx
@@ -20,8 +20,6 @@ const DynamicIcon = ({ className, type }: DynamicIconProps) => {
         style={{
           width: '100%',
           height: '100%',
-          // paddingLeft: '10px',
-          // paddingBottom: '10px',
         }}
       />
     </div>
