ed0a289992eee3249ddf8042d7278670f138dd12
seunghwan <sun1238799@naver.com>
2024-12-11 14:05:29 +0900

    refactor: 모달창 크기 통일

--- src/pages/Home/components/LoginModal.tsx
+++ src/pages/Home/components/LoginModal.tsx
@@ -1,5 +1,6 @@
 import DynamicIcon from '@/components/icon/DynamicIcon';
 import { Button } from '@/components/ui/button';
+import { motion } from 'framer-motion';
 import React, { useEffect, useRef } from 'react';
 
 interface LoginModalProps {
@@ -16,14 +17,24 @@ const LoginModal = ({ onSubmit }: LoginModalProps) => {
   }, []);
 
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="w-modal min-h-modal p-4  rounded-[10px] bg-modalWhite flex flex-col items-center justify-center">
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-30">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        transition={{
+          type: 'spring',
+          stiffness: 300,
+          damping: 20,
+        }}
+        className="w-modal h-modal max-h-modal p-4 rounded-[10px] bg-modalWhite flex flex-col items-center justify-center"
+      >
         <DynamicIcon className="w-[118px] h-[116px]" type="snow" />
-        <div className="my-[60px]">
+        <div className="my-[60px] flex-1">
           <div className="border-b-[1px] border-rgba(0, 0, 0, 0.5) mb-[10px]">
             <input
               ref={inputRef}
-              className="block  border-none placeholder:text-[#A1A1A1] focus:outline-none focus:border-none focus:ring-0"
+              className="block border-none placeholder:text-[#A1A1A1] focus:outline-none focus:border-none focus:ring-0"
               placeholder="이름을 입력해주세요."
             />
           </div>
@@ -31,7 +42,7 @@ const LoginModal = ({ onSubmit }: LoginModalProps) => {
           <Button className="w-[40px] h-[20px] rounded-[5px] text-[12px] bg-text-whiteDefault text-modalButtonBackground box-border mr-2">
             5기
           </Button>
-          <Button className="w-[29px] h-[13px] rounded-[5px] text-[12px] bg-text-whiteDefault text-modalButtonBackground box-border">
+          <Button className="w-[40px] h-[20px] rounded-[5px] text-[12px] bg-text-whiteDefault text-modalButtonBackground box-border mr-2">
             7기
           </Button>
         </div>
@@ -42,7 +53,7 @@ const LoginModal = ({ onSubmit }: LoginModalProps) => {
         >
           시작
         </Button>
-      </div>
+      </motion.div>
     </div>
   );
 };
--- src/pages/Home/components/WinningListModal.tsx
+++ src/pages/Home/components/WinningListModal.tsx
@@ -1,6 +1,7 @@
 import Icon from '@/components/icon/Icon';
 import Crown from '../../../assets/images/crown.png';
 import React from 'react';
+import { motion } from 'framer-motion';
 
 interface WinningListModalProps {
   onClose: () => void;
@@ -8,8 +9,18 @@ interface WinningListModalProps {
 
 const WinningListModal = ({ onClose }: WinningListModalProps) => {
   return (
-    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="max-w-[90%] w-[280px] relative min-h-[300px] p-4 rounded-[10px] bg-modalWhite flex flex-col items-center">
+    <div className="fixed inset-0 z-50 flex items-center justify-center">
+      <motion.div
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.9 }}
+        transition={{
+          type: 'spring',
+          stiffness: 300,
+          damping: 20,
+        }}
+        className="relative w-modal h-modal p-4 rounded-[10px] bg-modalWhite flex flex-col items-center justify-center"
+      >
         <div
           className="absolute rounded-full cursor-pointer top-4 right-4"
           onClick={onClose}
@@ -19,35 +30,37 @@ const WinningListModal = ({ onClose }: WinningListModalProps) => {
 
         <img className="w-[80px] h-[62px] mb-3" src={Crown} />
 
-        <div className="w-full rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
-          <div className="flex items-center h-[35px] justify-between gap-4 px-4">
-            <div>사진</div>
-            <span>스타벅스 10,000원 상품권</span>
-            <span>이상민</span>
+        <div className="flex-1 w-full overflow-y-scroll">
+          <div className="rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
+            <div className="flex items-center h-[35px] justify-between gap-4 px-4">
+              <div>사진</div>
+              <span>스타벅스 10,000원 상품권</span>
+              <span>이상민</span>
+            </div>
+          </div>
+          <div className="rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
+            <div className="flex items-center h-[35px] justify-between gap-4 px-4">
+              <div>사진</div>
+              <span>스타벅스 10,000원 상품권</span>
+              <span>이상민</span>
+            </div>
+          </div>
+          <div className="rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
+            <div className="flex items-center h-[35px] justify-between gap-4 px-4">
+              <div>사진</div>
+              <span>스타벅스 10,000원 상품권</span>
+              <span>이상민</span>
+            </div>
+          </div>
+          <div className="rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
+            <div className="flex items-center h-[35px] justify-between gap-4 px-4">
+              <div>사진</div>
+              <span>스타벅스 10,000원 상품권</span>
+              <span>이상민</span>
+            </div>
           </div>
         </div>
-        <div className="w-full rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
-          <div className="flex items-center h-[35px] justify-between gap-4 px-4">
-            <div>사진</div>
-            <span>스타벅스 10,000원 상품권</span>
-            <span>이상민</span>
-          </div>
-        </div>
-        <div className="w-full rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
-          <div className="flex items-center h-[35px] justify-between gap-4 px-4">
-            <div>사진</div>
-            <span>스타벅스 10,000원 상품권</span>
-            <span>이상민</span>
-          </div>
-        </div>
-        <div className="w-full rounded-[10px] border-[1px] border-[#A1A1A1] text-[12px] mb-[2px]">
-          <div className="flex items-center h-[35px] justify-between gap-4 px-4">
-            <div>사진</div>
-            <span>스타벅스 10,000원 상품권</span>
-            <span>이상민</span>
-          </div>
-        </div>
-      </div>
+      </motion.div>
     </div>
   );
 };
