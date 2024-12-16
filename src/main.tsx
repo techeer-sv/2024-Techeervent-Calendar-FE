370e1fa4837395062dd431c6f536be775507b425
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-16 18:38:47 +0900

    feat: 라우터 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,13 @@ import App from './App.tsx';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import Layout from './Layout.tsx';
-
-import ModalsProvider from './components/ModalsProvider.tsx';
-import Modals from './components/Modals.tsx';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <ModalsProvider>
-        <Layout>
-          <App />
-        </Layout>
-        <Modals />
-      </ModalsProvider>
+      <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>
