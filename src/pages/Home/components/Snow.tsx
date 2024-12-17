70afc18610fc8d2d711d3c088b456a1bc4484bf9
seunghwan <sun1238799@naver.com>
2024-12-17 22:50:51 +0900

    feat: 이스터에그 폭죽 구현

--- src/pages/Home/components/Snow.tsx
+++ src/pages/Home/components/Snow.tsx
@@ -1,23 +1,26 @@
 import { useEffect } from 'react';
 
-const SNOW_NUMBER = 40;
+const SNOW_NUMBER = 80;
 const MIN_DURATION = 20;
 const Snow = () => {
   const body = document.querySelector('body');
 
   useEffect(() => {
     const makeSnow = () => {
+      //6 ~ 10
+      const size = Math.random() * (15 - 6) + 6;
       const snowflake = document.createElement('div');
       const delay = Math.random();
-      const initialOpacity = Math.random();
+      const initialOpacity = Math.random() * (0.8 - 0.1) + 0.1;
       const duration = Math.random() * 10 + MIN_DURATION;
 
       snowflake.classList.add('snowflake');
       snowflake.style.left = `${Math.random() * window.innerWidth}px`;
       snowflake.style.animationDelay = `${delay}s`;
       snowflake.style.opacity = String(initialOpacity);
+      snowflake.style.width = `${size}px`;
+      snowflake.style.height = `${size}px`;
       snowflake.style.animation = `fall ${duration}s linear`;
-
       body?.appendChild(snowflake);
 
       setTimeout(
