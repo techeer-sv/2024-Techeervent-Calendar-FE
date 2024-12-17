1a40ed14a8edf642b1c9257cc5d9ae828245c04b
seunghwan <sun1238799@naver.com>
2024-12-17 21:54:48 +0900

    refactor: globals.css 삭제

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import '../globals.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from './Layout.tsx';
