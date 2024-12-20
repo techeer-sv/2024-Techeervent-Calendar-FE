fb31546837c45581620eec01ce9bbab683eb620d
seunghwan <sun1238799@naver.com>
2024-12-20 22:26:55 +0900

    fix: UI 피드백 반영

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -3,7 +3,7 @@ import SpriteSheet from '@/components/icon/SpriteSheet';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
-    <div className="w-full max-w-screen-lg min-h-screen mx-auto shadow-md bg-mainBackground">
+    <div className="w-full max-w-screen-lg min-h-screen mx-auto overflow-x-hidden shadow-md bg-mainBackground">
       {SpriteSheet}
       {children}
     </div>
--- src/pages/Home/components/Modals/QuestionCheckModal.tsx
+++ src/pages/Home/components/Modals/QuestionCheckModal.tsx
@@ -21,21 +21,39 @@ const QuestionCheckModal = ({ onClose }: QuestionModalProps) => {
         }}
         className="w-modal h-modal max-h-modal p-4 rounded-[10px] bg-modalWhite flex flex-col items-center justify-center"
       >
-        <div className="w-modal relative min-h-modal p-4 rounded-[10px] bg-modalWhite flex flex-col items-center justify-center">
-          <img className="w-[80px] h-[70px]" src={QandA} />
-          <h1 className="font-semibold text-[14px] mt-[32px] mb-[10px]">
-            Q) 2024년, 나를 가장 많이 웃게 한 일은?
-          </h1>
-          <div className="min-h-full overflow-scroll w-full p-2 text-[13px] rounded-[10px] bg-[#F3F3F3]  box-border leading-[1.1]">
-            코드에서 난생처음 어쩌구
-          </div>
-          <Button
-            onClick={onClose}
-            className="w-full text-[16px] rounded-[999px] cursor-pointer bg-modalButtonBackground my-[5px] text-whiteDefault box-border hover:opacity-70"
-          >
-            닫기
-          </Button>
+        <img className="w-[80px] h-[70px]" src={QandA} />
+        <h1 className="font-semibold text-[14px] mt-[32px] mb-[10px]">
+          Q) 2024년, 나를 가장 많이 웃게 한 일은?
+        </h1>
+        <div className="flex-grow overflow-y-scroll w-full p-2 text-[13px] rounded-[10px] bg-[#F3F3F3]  box-border leading-[1.1]">
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구
+          코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음
+          어쩌구 코드에서 난생처음 어쩌구 코드에서 난생처음 어쩌구 코드에서
+          난생처음 어쩌구
         </div>
+        <Button
+          onClick={onClose}
+          className="w-full text-[16px] rounded-[999px] cursor-pointer bg-modalButtonBackground my-[5px] text-whiteDefault box-border hover:opacity-70"
+        >
+          닫기
+        </Button>
       </motion.div>
     </div>
   );
