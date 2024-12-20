d2daf1627913bd965dbb198442a019f21fcc0457
Yejiin21 <101397075+Yejiin21@users.noreply.github.com>
2024-12-20 16:49:25 +0900

    design: 폰트, 컴포넌트 UI 수정

--- src/pages/Home/components/QnaCounter.tsx
+++ src/pages/Home/components/QnaCounter.tsx
@@ -19,7 +19,7 @@ const QnaCounter = () => {
   return (
     <div className="absolute flex sm:bottom-[6%] md:bottom-[5%] sm:-right-[8%] md:-right-[8%] lg:-right-[0%] w-[20%] h-[20%] rotate-[-23]">
       <img src={Board} alt="질의응답 카운트" />
-      <span className="absolute right-[36%] top-[22%] text-yellowText sm:text-[14px] md:text-[18px] lg:text-[20px] font-jua rotate-[23deg] z-10">
+      <span className="absolute right-[24%] top-[24%] text-yellowText sm:text-[14px] md:text-[18px] lg:text-[20px] font-jua rotate-[23deg] z-10">
         {answerCount !== null ? answerCount : 'Loading...'}
       </span>
     </div>
