e01781f2c793673ec8f904bdacbf26377017bc7b
jinoo <jinoo0306@naver.com>
2024-12-21 19:47:51 +0900

    feat : Layout에 BGM 컴포넌트 추가

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,9 +1,11 @@
 import { ReactNode } from 'react';
 import SpriteSheet from '@/components/icon/SpriteSheet';
+import BGM from '@/components/BGM';
 
 const Layout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="w-full max-w-screen-lg min-h-screen mx-auto overflow-x-hidden shadow-md bg-mainBackground">
+      <BGM />
       {SpriteSheet}
       {children}
     </div>
