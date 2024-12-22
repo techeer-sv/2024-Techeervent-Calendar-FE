f7b1babba57d26655b82af571a3801e4d379191a
jinoo <jinoo0306@naver.com>
2024-12-23 01:16:54 +0900

    feat : 출석을 하면 queryClient 객체를 이용하여 불러온 다른 내용들 refetch 하도록 기능 추가

--- src/pages/Home/components/QnaCounter.tsx
+++ src/pages/Home/components/QnaCounter.tsx
@@ -1,26 +1,20 @@
-import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
 import Board from '../../../../public/assets/Board.svg';
 import { fetchAnswerCount } from '@/services/calendar';
+import { GetAnswerCountResponse } from '@/types/api/calendar';
 
 const QnaCounter = () => {
-  const [answerCount, setAnswerCount] = useState<number | null>(null);
+  const { data, isFetching } = useQuery<GetAnswerCountResponse>({
+    queryKey: ['answerCount'],
+    queryFn: fetchAnswerCount,
+  });
 
-  useEffect(() => {
-    const getAnswerCount = async () => {
-      try {
-        const response = await fetchAnswerCount();
-        setAnswerCount(response.data.answerCount);
-      } catch {
-        return null;
-      }
-    };
-    getAnswerCount();
-  }, []);
   return (
     <div className="absolute flex sm:bottom-[8%] md:bottom-[11%] sm:-right-[8%] md:-right-[8%] lg:-right-[0%] w-[20%] h-[20%] rotate-[-23]">
       <img src={Board} alt="질의응답 카운트" />
       <span className="absolute right-[24%] top-[24%] text-yellowText sm:text-[14px] md:text-[18px] lg:text-[20px] font-jua rotate-[23deg] z-10">
-        {answerCount !== null ? answerCount : 'Loading...'}
+        {isFetching && 'Loading...'}
+        {!isFetching && data?.data.answerCount}
       </span>
     </div>
   );
