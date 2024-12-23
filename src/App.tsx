d9447138e7245834b9463b11a44f90987dbe6072
jinoo <jinoo0306@naver.com>
2024-12-23 23:08:54 +0900

    feat : 모바일은 BGM로직이 따로 적용되도록 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,40 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Outlet,
+} from 'react-router-dom';
 import Layout from './Layout.tsx';
 import ModalsProvider from './components/ModalsProvider.tsx';
 import Modals from './components/Modals.tsx';
 import Dino from './pages/Home/components/DinoGame.tsx';
 import Home from './pages/Home/Home.tsx';
+import BGM from './components/BGM.tsx';
+
+function AppLayout() {
+  return (
+    <ModalsProvider>
+      <Layout>
+        <Outlet />
+      </Layout>
+      <Modals />
+    </ModalsProvider>
+  );
+}
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <ModalsProvider>
-              <Layout>
-                <Home />
-              </Layout>
-              <Modals />
-            </ModalsProvider>
-          }
-        />
-        <Route path="*" element={<Dino />} />
-      </Routes>
-    </Router>
+    <div>
+      <BGM />
+      <Router>
+        <Routes>
+          <Route path="/" element={<AppLayout />}>
+            <Route index element={<Home />} />
+          </Route>
+          <Route path="*" element={<Dino />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
